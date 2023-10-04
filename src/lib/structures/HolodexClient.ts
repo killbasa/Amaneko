8d0e4fbe6b57c3e5061134cad6d324b3e08fdb1f
Shane <killbasab@gmail.com>
2023-10-04 19:38:07 -0400

    fix: only return videos that were searched for (#50)

--- src/lib/structures/HolodexClient.ts
+++ src/lib/structures/HolodexClient.ts
@@ -28,8 +28,8 @@ export class HolodexClient {
 
 		const result = await this.fetch<Holodex.VideoWithChannel[]>(url, this.apiKey);
 		if (maxUpcoming) {
-			return result.filter(({ available_at }) => {
-				return new Date(available_at).getTime() <= Date.now() + maxUpcoming;
+			return result.filter(({ channel, available_at }) => {
+				return new Date(available_at).getTime() <= Date.now() + maxUpcoming && channels.includes(channel.id);
 			});
 		}
 
