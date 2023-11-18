a73453acf45bac0d88b9d1cdc174c2f012ac139d
killbasa <killbasab@gmail.com>
2023-11-18 13:16:42 -0500

    fix: move to seperate files

--- src/commands/Relay/blacklist.ts
+++ src/commands/Relay/blacklist.ts
@@ -2,10 +2,9 @@ import { AmanekoSubcommand } from '#lib/extensions/AmanekoSubcommand';
 import { AmanekoError } from '#lib/structures/AmanekoError';
 import { BrandColors } from '#utils/constants';
 import { getUsername } from '#utils/youtube';
-import { errorReply, successReply } from '#lib/utils/reply';
-import { AmanekoCommand } from '#lib/extensions/AmanekoCommand';
+import { successReply } from '#lib/utils/reply';
 import { ApplyOptions } from '@sapphire/decorators';
-import { ApplicationCommandType, EmbedBuilder, PermissionFlagsBits } from 'discord.js';
+import { EmbedBuilder, PermissionFlagsBits } from 'discord.js';
 import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 import type { ApplicationCommandOptionChoiceData } from 'discord.js';
 import type { Blacklist } from '#lib/types/YouTube';
@@ -210,57 +209,5 @@ export class Command extends AmanekoSubcommand {
 		}
 
 		return embed;
-	}
-}
-
-@ApplyOptions<AmanekoCommand.Options>({
-	description: 'Manage the stream chat relay blacklist.',
-	runIn: ['GUILD_ANY']
-})
-export class ContextCommand extends AmanekoCommand {
-	public override registerApplicationCommands(registry: AmanekoCommand.Registry): void {
-		registry.registerContextMenuCommand((builder) =>
-			builder //
-				.setName('Blacklist User')
-				.setType(ApplicationCommandType.Message)
-				.setDMPermission(false)
-				.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
-		);
-	}
-
-	public override async contextMenuRun(interaction: AmanekoCommand.ContextMenuCommandInteraction): Promise<unknown> {
-		await interaction.deferReply();
-
-		const messageId = interaction.targetId;
-
-		const userToBlacklist = await this.container.prisma.streamComment.findUnique({
-			where: { messageId },
-			select: { channelId: true }
-		});
-		if (!userToBlacklist?.channelId) {
-			return errorReply(interaction, 'This message is either not a stream comment or the stream is over.');
-		}
-
-		const blacklistedUsername = await getUsername(userToBlacklist.channelId);
-
-		const blacklistedUser = await this.container.prisma.blacklist.upsert({
-			where: { channelId_guildId: { channelId: userToBlacklist.channelId, guildId: interaction.guildId } },
-			update: {
-				channelId: userToBlacklist.channelId,
-				channelName: blacklistedUsername
-			},
-			create: {
-				channelId: userToBlacklist.channelId,
-				channelName: blacklistedUsername,
-				guild: {
-					connectOrCreate: {
-						where: { id: interaction.guildId },
-						create: { id: interaction.guildId }
-					}
-				}
-			}
-		});
-
-		return successReply(interaction, `Added **${blacklistedUser.channelName}** to the blacklist.`);
 	}
 }
