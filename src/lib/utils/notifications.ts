d009c7799f3207683e911e3dab1644fee2e7bdb0
Shane <killbasab@gmail.com>
2023-11-25 18:39:46 -0500

    fix: filter comments properly (#115)
    * fix: filter comments properly
    
    * chore: more cases

--- src/lib/utils/notifications.ts
+++ src/lib/utils/notifications.ts
@@ -1,3 +1,4 @@
+import { container } from '@sapphire/framework';
 import type { Holodex } from '#lib/types/Holodex';
 import type { TLDex } from '#lib/types/TLDex';
 import type { Prisma } from '@prisma/client';
@@ -23,4 +24,36 @@ export function resolveRelayQuery(comment: TLDex.CommentPayload, video: Holodex.
 	}
 
 	return query;
+}
+
+export function shouldFilterComment(cmt: TLDex.CommentPayload, video: Holodex.VideoWithChannel): boolean {
+	const { logger } = container;
+
+	// Filter out verified-only channels
+	// TODO: Allow certain verified channels?
+	if (cmt.is_verified && !cmt.is_vtuber && !cmt.is_tl && !cmt.is_moderator) {
+		if (process.env.NODE_ENV !== 'test') {
+			logger.debug(`[Relay] Filtered out verified comment from ${cmt.name} (${video.channel.id})`, {
+				isOwner: cmt.is_owner,
+				isVTuber: cmt.is_vtuber,
+				isTL: cmt.is_tl,
+				isMod: cmt.is_moderator
+			});
+		}
+
+		return true;
+	}
+
+	// Filter out Super Chat heart messages
+	if (cmt.is_owner || cmt.channel_id === video.channel.id) {
+		if (cmt.message.startsWith('hearted a Super Chat from')) {
+			if (process.env.NODE_ENV !== 'test') {
+				logger.debug(`[Relay] Filtered out Super Chat heart message from ${cmt.name} (${video.channel.id})`);
+			}
+
+			return true;
+		}
+	}
+
+	return false;
 }
