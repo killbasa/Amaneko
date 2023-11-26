c38aa03aaa88c2ce09514ffec791fc4763608cc7
killbasa <killbasab@gmail.com>
2023-11-26 13:17:59 -0500

    fix: add augments

--- src/lib/types/Augments.ts
+++ src/lib/types/Augments.ts
@@ -1,10 +1,14 @@
 import type { ClientConfig } from '#lib/types/Config';
+import type { AmanekoEvents } from '#lib/utils/enums';
 import type { Primitive, RedisClient } from '@killbasa/redis-utils';
 import type { HolodexChannel, PrismaClient } from '@prisma/client';
+import type { CommunityPostData } from '#lib/types/YouTube';
 import type { MeiliClient } from '#lib/extensions/MeiliClient';
 import type { HolodexClient } from '#lib/structures/HolodexClient';
 import type { TLDexClient } from '#lib/structures/TLDexClient';
 import type { Collection } from 'discord.js';
+import type { TLDex } from '#lib/types/TLDex';
+import type { Holodex } from '#lib/types/Holodex';
 import type { MetricsClient } from '#lib/structures/MetricsClient';
 import type { youtube_v3 } from 'googleapis';
 import type { OpenTelemetryClient } from '#lib/structures/OpenTelemetryClient';
@@ -13,6 +17,14 @@ import type { NotifierStore } from '#lib/extensions/NotifierStore';
 
 declare module 'discord.js' {
 	interface Client {}
+
+	interface ClientEvents {
+		[AmanekoEvents.CommunityPost]: [post: CommunityPostData];
+		[AmanekoEvents.StreamPrechat]: [video: Holodex.VideoWithChannel];
+		[AmanekoEvents.StreamStart]: [video: Holodex.VideoWithChannel];
+		[AmanekoEvents.StreamEnd]: [video: Holodex.VideoWithChannel];
+		[AmanekoEvents.StreamComment]: [message: TLDex.CommentPayload, video: Holodex.VideoWithChannel];
+	}
 }
 
 declare module '@sapphire/framework' {
